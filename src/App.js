8a34081f8ee893c5ee6e4cb8603dda036a90507a
karenbarseghyan <[karenbarseghyan90@gmail.com]>
2020-09-04 01:11:10 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Changes for new branch
         </p>
         <a
           className="App-link"
