c1a1ba9faa59221d9068671f2409353ec070bcfc
karenbarseghyan <[karenbarseghyan90@gmail.com]>
2020-09-04 01:06:13 +0400

    fixed index

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Changes for new branch
         </p>
-        <p> Bye  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
