0190b86f84aebaec3396f7d5ba7b66dfbd2f6240
karenbarseghyan <[karenbarseghyan90@gmail.com]>
2020-09-07 01:20:24 +0400

    Adding function components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import Summ, {multiply} from "./Example.js";
+//import sayHello from "./test";
+
+function Person (properties) {
+  console.log(properties);
+  return (
+    <div>
+      <h1> Hello</h1>
+      <p>My name is Karen </p>
+    </div>
+  );
+}
+
+
 
 function App() {
+  /*console.log(Summ(2,4));
+  console.log(multiply(2,4));
+  function sum(a,b){
+    return a + b;
+  };
+  console.log(sum(2,4));
+  console.log(sayHello()); */
   return (
     <div className="App">
       <header className="App-header">
+        <Person surname = "Barseghyan" />
+        <Person />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p> Bye  </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +41,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
---
+++ src/Example.js
@@ -0,0 +1,9 @@
+function sum(a,b){
+    return a + b;
+};
+
+export const multiply = (a,b) => {
+    return a*b;
+};
+
+export default sum;
